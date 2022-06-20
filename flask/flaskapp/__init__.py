1974d5fb9256391e1745ee5819e10999904d6948
Anthony Salemo <ajssalemo@gmail.com>
2022-06-20 15:58:56 -0400

    updating example in the flask name route

--- flask/flaskapp/__init__.py
+++ flask/flaskapp/__init__.py
@@ -8,5 +8,5 @@ def index():
 
 @app.route("/hello/<name>")
 def hello(name: str):
-    return jsonify({ "message": f"Hello, {name} from python-wsgi-function-samples-flask" })
+    return jsonify({ "message": f"Hello {name}, from python-wsgi-function-samples-flask" })
 
